9890b3e1be03f057fbe2572480e77199f7cd5492
Amitabha2001 <amiabharoy4@gmail.com>
2015-02-08 15:38:36 +0530

    Update 1

--- AmiOS/AmiOS/Make.bat
+++ AmiOS/AmiOS/Make.bat
@@ -1,63 +1,62 @@
 echo ------------------- AmiOS Build System --------------------
 
 REM Lets Assemble the Assembler Programs
-cd C:\MASM\BIN
-ml /AT /c C:\AmiOS\AmiOS\KernelEntry.asm
+cd ...\MASM\BIN
+ml /AT /c ...\KernelEntry.asm
 
-cd C:\NASM
-nasm -f bin C:\AmiOS\AmiOS\Boot.asm -o C:\AmiOS\AmiOS\Build\BootLoader.bin
-nasm -f bin C:\AmiOS\AmiOS\Shell.asm -o C:\AmiOS\AmiOS\Build\Shellii.bin
-REM nasm -f obj C:\AmiOS\AmiOS\Interrupts.asm -o C:\AmiOS\AmiOS\Interrupts.obj
+cd ...\NASM
+nasm -f bin ...\Boot.asm -o ...\Build\BootLoader.bin
+nasm -f bin ...\Shell.asm -o ...\Build\Shellii.bin
 
 REM Lets Compile the C Programs
-cd C:\MSVC\MSVC15\BIN
-cl /AT /Gs /Gx /c /Zl C:\AmiOS\AmiOS\Kernel.cpp
-cl /AT /Gs /Gx /c /Zl C:\AmiOS\AmiOS\Display.cpp
-cl /AT /Gs /Gx /c /Zl C:\AmiOS\AmiOS\Disk.cpp
-cl /AT /Gs /Gx /c /Zl C:\AmiOS\AmiOS\String.cpp
-cl /AT /Gs /Gx /c /Zl C:\AmiOS\AmiOS\System.cpp
-cl /AT /Gs /Gx /c /Zl C:\AmiOS\AmiOS\Memory.cpp
-cl /AT /Gs /Gx /c /Zl C:\AmiOS\AmiOS\Kernel.cpp
-cl /AT /Gs /Gx /c /Zl C:\AmiOS\AmiOS\Process.cpp
+cd ...\MSVC\BIN
+cl /AT /Gs /Gx /c /Zl ...\Kernel.cpp
+cl /AT /Gs /Gx /c /Zl ...\Display.cpp
+cl /AT /Gs /Gx /c /Zl ...\Disk.cpp
+cl /AT /Gs /Gx /c /Zl ...\String.cpp
+cl /AT /Gs /Gx /c /Zl ...\System.cpp
+cl /AT /Gs /Gx /c /Zl ...\Memory.cpp
+cl /AT /Gs /Gx /c /Zl ...\Kernel.cpp
+cl /AT /Gs /Gx /c /Zl ...\Process.cpp
 
 REM Copy all object files.
-copy /b C:\MASM\BIN\KernelEntry.obj C:\AmiOS\AmiOS\KernelEntry.obj
-copy /b C:\MSVC\MSVC15\BIN\Kernel.obj  C:\AmiOS\AmiOS\Kernel.obj
-copy /b C:\MSVC\MSVC15\BIN\Display.obj C:\AmiOS\AmiOS\Display.obj
-copy /b C:\MSVC\MSVC15\BIN\Disk.obj C:\AmiOS\AmiOS\Disk.obj
-copy /b C:\MSVC\MSVC15\BIN\String.obj C:\AmiOS\AmiOS\String.obj
-copy /b C:\MSVC\MSVC15\BIN\System.obj C:\AmiOS\AmiOS\System.obj
-copy /b C:\MSVC\MSVC15\BIN\Memory.obj C:\AmiOS\AmiOS\Memory.obj
-copy /b C:\MSVC\MSVC15\BIN\Process.obj C:\AmiOS\AmiOS\Process.obj
+copy /b ...\MSVC\BIN\KernelEntry.obj ...\KernelEntry.obj
+copy /b	...\MSVC\BIN\Kernel.obj  ...\Kernel.obj
+copy /b ...\MSVC\BIN\Display.obj ...\Display.obj
+copy /b ...\MSVC\BIN\Disk.obj ...\Disk.obj
+copy /b ...\MSVC\BIN\String.obj ...\String.obj
+copy /b ...\MSVC\BIN\System.obj ...\System.obj
+copy /b ...\MSVC\BIN\Memory.obj ...\Memory.obj
+copy /b ...\MSVC\BIN\Process.obj ...\Process.obj
 
 REM Call Linker.
-cd C:\MASM\BIN
-link /T /MAP /NOD C:\AmiOS\AmiOS\KernelEntry.obj C:\AmiOS\AmiOS\Kernel.obj C:\AmiOS\AmiOS\Display.obj C:\AmiOS\AmiOS\Disk.obj C:\AmiOS\AmiOS\String.obj C:\AmiOS\AmiOS\System.obj C:\AmiOS\AmiOS\Memory.obj C:\AmiOS\AmiOS\Process.obj
+cd ...\MASM\BIN
+link /T /MAP /NOD ...\KernelEntry.obj ...\Kernel.obj ...\Display.obj ...\Disk.obj ...\String.obj ...\System.obj ...\Memory.obj ...\Process.obj
 copy /b C:\MASM\BIN\KernelEntry.com C:\AmiOS\AmiOS\Build\Kernel.com
 
 REM Make our floppy image.
-cd C:\FatX
-FATX -b C:\AmiOS\AmiOS\Build\BootLoader.bin -k C:\AmiOS\AmiOS\Build\Kernel.com -o C:\AmiOS\AmiOS\Build\AmiOS.img C:\AmiOS\AmiOS\Build\Shellii.bin
+cd ...\FatX
+FATX -b ...\Build\BootLoader.bin -k ...\Build\Kernel.com -o ...\Build\AmiOS.img ...\Build\Shellii.bin
 
 REM Add the magic bytes with the file.
 
 REM Delete Object Files.
-del C:\AmiOS\AmiOS\KernelEntry.obj
-del C:\AmiOS\AmiOS\Kernel.obj
-del C:\AmiOS\AmiOS\Display.obj
-del C:\AmiOS\AmiOS\Disk.obj
-del C:\AmiOS\AmiOS\String.obj
-del C:\AmiOS\AmiOS\System.obj
-del C:\AmiOS\AmiOS\Memory.obj
-del C:\AmiOS\AmiOS\Process.obj
-del C:\MASM\BIN\KernelEntry.obj
-del C:\MSVC\MSVC15\BIN\Kernel.obj
-del C:\MSVC\MSVC15\BIN\Display.obj
-del C:\MSVC\MSVC15\BIN\Disk.obj
-del C:\MSVC\MSVC15\BIN\String.obj
-del C:\MSVC\MSVC15\BIN\System.obj
-del C:\MSVC\MSVC15\BIN\Memory.obj
-del C:\MSVC\MSVC15\BIN\Process.obj
+del ...\KernelEntry.obj
+del ...\Kernel.obj
+del ...\Display.obj
+del ...\Disk.obj
+del ...\String.obj
+del ...\System.obj
+del ...\Memory.obj
+del ...\Process.obj
+del ...\MASM\BIN\KernelEntry.obj
+del ...\MSVC\BIN\Kernel.obj
+del ...\MSVC\BIN\Display.obj
+del ...\MSVC\BIN\Disk.obj
+del ...\MSVC\BIN\String.obj
+del ...\MSVC\BIN\System.obj
+del ...\MSVC\BIN\Memory.obj
+del ...\MSVC\BIN\Process.obj
 
 cd C:\Bochs
 bochs -f C:\AmiOS\AmiOS\Build\boschsrc.bxrc
--- AmiOS/AmiOS/include/String.h
+++ AmiOS/AmiOS/include/String.h
@@ -13,7 +13,7 @@ struct String
 	int16_t strlen(const char far * str);
 	bool strcmp(const char far * str1,const char far  * str2);
 	char far * strcpy(const char far * dest,const char far * src);
-	bool str_substr(const char far * src,int16_t start_pos,int16_t end_pos);
+	char far * str_substr(const char far * src,int16_t start_pos,int16_t end_pos);
 	int16_t str_getchar_pos(const char far * src,char ch);
 	bool str_setchar(char far  * src,int16_t pos,char ch);
 };
